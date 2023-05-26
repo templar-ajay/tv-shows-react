5570bfc535a0dcab32b0122ac138e96bbd1c5566
templar-ajay <templar.ajay@gmail.com>
2023-05-27 05:04:00 +0530

    pata nahi is commit me kya kiya h mne

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-// import TVShowsList from "./components/tvShowsList";
+import TVShowsList from "./components/tvShowsList";
 import "./App.css";
 import ShowDetails from "./components/ShowDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // return <TVShowsList />;
-  return <ShowDetails />;
+  // return <ShowDetails />;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/tv-shows" element={<TVShowsList />}>
+      <Route path="/tv-shows/details/" element={<ShowDetails />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
---
+++ src/pages/NoPage.js
@@ -0,0 +1,3 @@
+export default function noPage() {
+  return <>page not found</>;
+}
